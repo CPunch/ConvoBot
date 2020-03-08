98d3160d4b89773406e539803f0a446b71860bfe
CPunch <sethtstubbs@gmail.com>
2020-03-07 23:17:54 -0600

    memory optimizations

--- train.py
+++ train.py
@@ -12,11 +12,9 @@ if not os.path.isdir(os.path.join("models", model_name)):
 	print(f"Downloading {model_name} model...")
 	gpt2.download_gpt2(model_name=model_name)   # model is saved into current directory under /models/124M/
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
 sess = gpt2.start_tf_sess()
 
 # start train
-gpt2.finetune(sess, main_dataset, model_name=model_name, steps=10000) # steps is max number of training steps
+gpt2.finetune(sess, main_dataset, accumulate_gradients=1, use_memory_saving_gradients=True, model_name=model_name, steps=10000) # steps is max number of training steps
 
 print(gpt2.generate(sess))
