ee9c23dd67fc5f53ed9b75ecf6bb9ff3ac8f1c22
CPunch <sethtstubbs@gmail.com>
2020-03-07 23:19:03 -0600

    better setup comments

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 # requires pew to be installed with pip (virtual env stuff)
 pip install pew
-# THIS REQUIRES TENSORFLOW 1.15
+# THIS REQUIRES TENSORFLOW 1.15 (or 1.13.1 if you have a lower-end gpu), CUDA 10.0, AND CUDNN 10.0
 pew new --python=3.7 -i discord.py -i tensorflow-gpu==1.15 -i gpt-2-simple ConvoBot
