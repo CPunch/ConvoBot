244db21bcdcd417e6cbb978039dde72d92c4725f
CPunch <sethtstubbs@gmail.com>
2020-03-08 01:56:49 -0600

    should get better responses

--- bot.py
+++ bot.py
@@ -44,11 +44,16 @@ class Conversation:
             output = output + reply + "\n"
         return output
 
+    def getUnique(self, results):
+        for res in results:
+            if not res in self.conversation:
+                return res
+
     def buildResponse(self):
-        rawresponse = gpt2.generate(sess, prefix=self.grabText(), include_prefix=False, length=20, return_as_list=True)
+        rawresponse = gpt2.generate(sess, prefix=self.grabText(), include_prefix=False, length=75, return_as_list=True)
         print(rawresponse)
         chats = rawresponse[0].split('\n')
-        return chats[1] + " " + chats[2] 
+        return getUnique(chats)
 
 @client.event
 async def on_ready():
